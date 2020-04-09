a71a6bf3b9fa0ece2cf2c642e15b969fbd66ed9a
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-09 19:16:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,14 +14,12 @@ class Book
   def page_count=(num)
     @page_count = num
   end
-
   def page_count
     @page_count
   end
-  ef genre=(genre)
+  def genre=(genre)
     @genre = genre
   end
-
   def genre
     @genre
   end
